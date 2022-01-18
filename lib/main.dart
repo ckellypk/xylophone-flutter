c4ccc24db8de67c1be561c171830ace90b1f9aff
ckellypk <ckelly@commure.com>
2022-01-18 11:14:26 -0500

    notes complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,17 @@
 import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 //import 'package:english_words/english_words.dart';
 //import 'package:audioplayers/audio_cache.dart';
 /*
  *
- * This is the starter project. We are adding a few packages to additional
- * functionality. You get packages here: https://pub.dev/packages
+ * Now that we have the app functioning lets refactor to use a function to
+ * return our individual widgets.
  *
- * In this case we are adding the package to play the audio files. The files
- * are already imported for you in the pubspec.yaml.
+ * We create the function build key to return our individual keys. Notice how
+ * the parameters are named and we can pass in the specific class member when
+ * setting widget properties
  *
- * We create each widget to play our xylophone app but it looks like the same
- * widget structure repeated over and over. Next  branch shows how to reduce this
- * repeated code.
  *
   */
 
@@ -25,6 +24,23 @@ class XylophoneApp extends StatelessWidget {
     player.play(soundPlayed);
   }
 
+  Expanded buildKey({String soundFile, Color colorPicked}){
+    return Expanded(
+      child: TextButton(
+          onPressed: () {
+            playSound(soundFile);
+          },
+          child: Text(''),
+          style: TextButton.styleFrom(
+              shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.zero)
+              ),
+              backgroundColor: colorPicked
+          )
+      ),
+    );
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -35,109 +51,119 @@ class XylophoneApp extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              Expanded(
-                child: TextButton(
-                  onPressed: () {
-                    playSound('note1.wav');
-                  },
-                  child: Text(''),
-                  style: TextButton.styleFrom(
-                     shape: const RoundedRectangleBorder(
-                       borderRadius: BorderRadius.all(Radius.zero)
-                    ),
-                    backgroundColor: Colors.red
-                  )
-                ),
-              ),
-              Expanded(
-                child: TextButton(
-                  onPressed: () {
-                    playSound('note2.wav');
-                  },
-                  child: Text(''),
-                  style: TextButton.styleFrom(
-                      shape: const RoundedRectangleBorder(
-                          borderRadius: BorderRadius.all(Radius.zero)
-                      ),
-                      backgroundColor: Colors.orange
-                  )
-                ),
-              ),
-              Expanded(
-                child: TextButton(
-                  onPressed: () {
-                    playSound('note3.wav');
-                  },
-                  child: Text(''),
-                  style: TextButton.styleFrom(
-                      shape: const RoundedRectangleBorder(
-                          borderRadius: BorderRadius.all(Radius.zero)
-                      ),
-                      backgroundColor: Colors.yellow
-                  )
-                ),
-              ),
-              Expanded(
-                child: TextButton(
-                  onPressed: () {
-                    playSound('note4.wav');
-                  },
-                  child: Text(''),
-                  style: TextButton.styleFrom(
-                      shape: const RoundedRectangleBorder(
-                          borderRadius: BorderRadius.all(Radius.zero)
-                      ),
-                      backgroundColor: Colors.green
-                  )
-                ),
-              ),
-              Expanded(
-                child: TextButton(
-                  onPressed: () {
-                   playSound('note5.wav');
-                  },
-                  child: Text(''),
-                  style: TextButton.styleFrom(
-                      shape: const RoundedRectangleBorder(
-                          borderRadius: BorderRadius.all(Radius.zero)
-                      ),
-                      backgroundColor: Colors.teal
-                  )
-                ),
-              ),
-              Expanded(
-                child: TextButton(
-                  onPressed: () {
-                   playSound('note6.wav');
-                  },
-                  child: Text(''),
-                  style: TextButton.styleFrom(
-                      shape: const RoundedRectangleBorder(
-                          borderRadius: BorderRadius.all(Radius.zero)
-                      ),
-                      backgroundColor: Colors.blue
-                  )
-                ),
-              ),
-              Expanded(
-                child: TextButton(
-                  onPressed: () {
-                    playSound('note7.wav');
-                  },
-                  child: Text(''),
-                  style: TextButton.styleFrom(
-                      shape: const RoundedRectangleBorder(
-                          borderRadius: BorderRadius.all(Radius.zero)
-                      ),
-                      backgroundColor: Colors.purple
-                  )
-                ),
-              ),
+              buildKey(soundFile: 'note1.wav', colorPicked: Colors.red),
+              buildKey(soundFile: 'note2.wav', colorPicked: Colors.orange),
+              buildKey(soundFile: 'note3.wav', colorPicked: Colors.yellow),
+              buildKey(soundFile: 'note4.wav', colorPicked: Colors.green),
+              buildKey(soundFile: 'note5.wav', colorPicked: Colors.teal),
+              buildKey(soundFile: 'note6.wav', colorPicked: Colors.blue),
+              buildKey(soundFile: 'note7.wav', colorPicked: Colors.purple),
             ],
-
           ),
         ),
       ),
     );
   }
 }
+
+
+
+/*
+Expanded(
+child: TextButton(
+onPressed: () {
+playSound('note1.wav');
+},
+child: Text(''),
+style: TextButton.styleFrom(
+shape: const RoundedRectangleBorder(
+borderRadius: BorderRadius.all(Radius.zero)
+),
+backgroundColor: Colors.red
+)
+),
+),
+Expanded(
+child: TextButton(
+onPressed: () {
+playSound('note2.wav');
+},
+child: Text(''),
+style: TextButton.styleFrom(
+shape: const RoundedRectangleBorder(
+borderRadius: BorderRadius.all(Radius.zero)
+),
+backgroundColor: Colors.orange
+)
+),
+),
+Expanded(
+child: TextButton(
+onPressed: () {
+playSound('note3.wav');
+},
+child: Text(''),
+style: TextButton.styleFrom(
+shape: const RoundedRectangleBorder(
+borderRadius: BorderRadius.all(Radius.zero)
+),
+backgroundColor: Colors.yellow
+)
+),
+),
+Expanded(
+child: TextButton(
+onPressed: () {
+playSound('note4.wav');
+},
+child: Text(''),
+style: TextButton.styleFrom(
+shape: const RoundedRectangleBorder(
+borderRadius: BorderRadius.all(Radius.zero)
+),
+backgroundColor: Colors.green
+)
+),
+),
+Expanded(
+child: TextButton(
+onPressed: () {
+playSound('note5.wav');
+},
+child: Text(''),
+style: TextButton.styleFrom(
+shape: const RoundedRectangleBorder(
+borderRadius: BorderRadius.all(Radius.zero)
+),
+backgroundColor: Colors.teal
+)
+),
+),
+Expanded(
+child: TextButton(
+onPressed: () {
+playSound('note6.wav');
+},
+child: Text(''),
+style: TextButton.styleFrom(
+shape: const RoundedRectangleBorder(
+borderRadius: BorderRadius.all(Radius.zero)
+),
+backgroundColor: Colors.blue
+)
+),
+),
+Expanded(
+child: TextButton(
+onPressed: () {
+playSound('note7.wav');
+},
+child: Text(''),
+style: TextButton.styleFrom(
+shape: const RoundedRectangleBorder(
+borderRadius: BorderRadius.all(Radius.zero)
+),
+backgroundColor: Colors.purple
+)
+),
+),*/
